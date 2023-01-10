44eacd917dcd1550896e6865cf8b13f5ed7fcf6a
Mohamad <m.elharoufi@hotmail.com>
2023-01-10 00:16:21 -0500

    new feature

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ dark_button.addEventListener("click", function() {
   document.querySelector('.answer_box').classList.toggle('night');
   document.querySelector('.switch').classList.toggle('night');
   document.querySelector('.title-name').classList.toggle('night');
+
+  //alternate between moon and sun images on every click
   if (nums %2 === 0){
     document.querySelector('.moon-image').src="images/sun.png";}
   else{
@@ -15,13 +17,7 @@ dark_button.addEventListener("click", function() {
   nums++;
   })
  
-
-
-
- 
-    
-
-//currencies
+//assign each element to a constant
 const select1 = document.querySelector('.currencies1');
 const select2 = document.querySelector('.currencies2');
 const convert_button = document.querySelector('.convert');
@@ -31,14 +27,15 @@ const result_num = document.querySelector('.result');
 const switch_button = document.querySelector('.switch');
   
     
+function fetch_data(){
+  fetch('https://api.frankfurter.app/currencies')
+  .then((data) => data.json())
+  .then((data) =>{
+    display(data);
+  });
+}
 
-
-
-fetch('https://api.frankfurter.app/currencies')
-.then((data) => data.json())
-.then((data) =>{
-  display(data);
-});
+fetch_data();
 
 function display(data){
   const entries = Object.entries(data);
@@ -56,7 +53,11 @@ switch_button.addEventListener("click", function(){
 
 });
 
-
+switch_button.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+  }
+});
 
 convert_button.addEventListener("click", function() {
   let currency1 = select1.value;
@@ -66,15 +67,29 @@ convert_button.addEventListener("click", function() {
   if (currency1 != currency2){
     convert(currency1,currency2,value)
   }else{
-    alert("You are trying to convert the same currency!\nPlease pick different currencies.");
+
+    alert("You are trying to convert the same currency!\n Please choose different currencies.")
+  }
+});
+
+//alows currency conversion with "enter" button
+document.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    // Trigger the button element with a click
+    convert_button.click();
   }
 });
 
 function convert(currency1,currency2,value){
-  const host = 'api.frankfurter.app';
-fetch(`https://${host}/latest?amount=${value}&from=${currency1}&to=${currency2}`)
-  .then(resp => resp.json())
-  .then((data) => {
-    current_num.innerHTML = value + '&nbsp;' + currency1 +'&nbsp' + '&nbsp' + '=';
-    result_num.innerHTML = `&nbsp ${Object.values(data.rates)[0].toFixed(2)} ${currency2}`; 
-  })};
+    const host = 'api.frankfurter.app';
+
+     fetch(`https://${host}/latest?amount=${value}&from=${currency1}&to=${currency2}`)
+    .then(resp => resp.json())
+    .then((data) => {
+      current_num.innerHTML = value + '&nbsp;' + currency1 +'&nbsp' + '&nbsp' + '=';
+      if (Object.values(data.rates)[0].toString().split(".")[1].length > 1){
+      result_num.innerHTML = `&nbsp ${Object.values(data.rates)[0].toFixed(2)} ${currency2}`; }
+      else{
+        result_num.innerHTML = `&nbsp ${Object.values(data.rates)[0]} ${currency2}`;
+      }
+    })};
